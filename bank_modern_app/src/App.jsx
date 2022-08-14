dedb5546e6bebdc7bae7f65f167aca8bf7f2db14
syrymumbetov <umbetov.syrym@gmail.com>
2022-08-14 15:49:24 +0600

    4: navbar

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import styles from "./style";
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.padding} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats Business CardDeal Testimonials Clients CTA Footer
+          <Stats /> <Business /> <CardDeal /> <Testimonials /> <Clients />{" "}
+          <CTA /> <Footer />
         </div>
       </div>
     </div>
